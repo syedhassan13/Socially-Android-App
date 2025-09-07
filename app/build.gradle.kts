e4a91d603503e00422d58e1c190e52f97fd104ff
Syed Muhammad Hassan <syedmuhammadhassan884@gmail.com>
2025-09-07 07:13:00 +0500

    Initial commit - Socially Android App

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.hassan.i230519"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.hassan.i230519"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("androidx.core:core:1.12.0")
+    implementation ("androidx.core:core-ktx:1.12.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // AGP >= 8.9.1 (8.9.3 is fine)
+    id("com.android.application") version "8.13.0" apply false
+    id("com.android.library")     version "8.13.0" apply false
+    // Keep Kotlin plugin up-to-date too
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+}
