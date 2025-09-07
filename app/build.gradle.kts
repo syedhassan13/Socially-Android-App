6cb18243749d11eed1091d262bfa9e55ffa5480f
Syed Muhammad Hassan <syedmuhammadhassan884@gmail.com>
2025-09-08 04:52:43 +0500

    SignUp page first part done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation ("androidx.core:core:1.12.0")
     implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
