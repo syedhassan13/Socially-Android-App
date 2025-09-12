7d31c93db29ac03201a9ca24eafaaa2bf39a0b99
Syed Muhammad Hassan <syedmuhammadhassan884@gmail.com>
2025-09-12 07:11:19 +0500

    Main Home Screen done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("com.google.android.material:material:1.12.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
